ac20671bbe8a828f7b1d873b2610a714445f048b
vantari1771-svg <vantari1771@gmail.com>
2025-11-16 09:59:37 -0600

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -12,7 +12,7 @@ st.set_page_config(
     page_icon="🤖",
     layout="centered",
 )
-st.title("💬 Generative AI Chatbot")
+st.title("💬 Vin Generative AI Chatbot")
 
 # initiate chat history
 if "chat_history" not in st.session_state:
